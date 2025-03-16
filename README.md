de43720407cac27122cce147497b87724697b679
rahman-najeebur <rahman.najeebur01@gmail.com>
2025-03-16 22:12:57 +0100

    first version of hangman

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# hangman
+# Hangman
+### Author: Najeeb Ur Rahman
+### Description:
+Hangman is a guessing game for two or more players. In our case the program generate a random word, we have to guess it with a certain number of attempt which is LIVES = 6 (in our program)
+<br>At each stage, when we lose a life the program will print an ASCII art in order to show the hangman stages
---
+++ main.py
@@ -0,0 +1,54 @@
+import random
+import hangman_words
+import hangman_art
+from hangman.hangman_art import stages
+
+LIVES = 6
+
+# TODO-01: print the hangman logo
+print(hangman_art.logo)
+
+# TODO-02: generate a random word
+word = random.choice(hangman_words.word_list)
+print(word)
+
+# TODO-03: create blanks spaces
+display = ""
+for letter in word:
+    display += "-"
+
+print(f"word to guess: {display}")
+
+# TODO-04: create a list which keep track of the correct letter placement.
+correct_list = []
+count = LIVES
+
+# TODO-05: main logic
+is_game_on = True
+while is_game_on:
+    # ask the user input
+    guess_letter = input("Guess a letter: ").lower()
+
+    display = ""
+    if guess_letter in word:
+        for letter in word:
+            if letter == guess_letter:
+                display += letter
+                correct_list.append(letter)
+            elif letter in correct_list:
+                display += letter
+            else:
+                display += "-"
+
+        print(display)
+        print(hangman_art.stages[len(stages) - 1])
+        if "-" not in display:
+            is_game_on = False
+
+    else:
+        count -= 1
+        print("You Guess a wrong Letter which is not in word. You Lose a Life.")
+        print(hangman_art.stages[count])
+        print(f"{15 * "*"} {count} / {LIVES} LIVES LEFT {15 * "*"}")
+        if count <= 0:
+            is_game_on =  False
